53a996762f4231298c59bed5868c24aa99cf692d
Regino-Kamberaj <regino.kamberaj@stud.unifi.it>
2023-06-16 22:48:01 +0200

    testing

--- my_django_project/settings.py
+++ my_django_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-94imr)0g$^!agi^p)$we%xauz@i=5ks0bsyz=reyn^a2j4_x3*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'mysocialmedia-app.up.railway.app'
